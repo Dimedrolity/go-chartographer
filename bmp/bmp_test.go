c581e373305feef5dab52c47a4037963940fa64f
Dmitry Redkin <dima.redkin.1999@gmail.com>
2022-02-23 16:53:19 +0500

    test "golang.org/x/image/bmp" library. Seems good

--- bmp/bmp_test.go
+++ bmp/bmp_test.go
@@ -1,7 +1,10 @@
 package bmp
 
 import (
+	"bytes"
 	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/image/bmp"
+	"os"
 	"testing"
 )
 
@@ -10,6 +13,22 @@ func TestEmptyImg(t *testing.T) {
 		imgBytes := createBmp(2, 3)
 		So(len(imgBytes), ShouldEqual, 78)
 	})
+}
 
-	//os.WriteFile("img.bmp", img, 0777)
+// TestBMPLib - интеграционный тест библиотеки "golang.org/x/image/bmp" для того, чтобы повысить доверие.
+// Библиотека должна кодировать/декодировать верно,
+// должны совпадать реальные с ожидаемыми: размер файла, размер изображений, пиксели - все байты.
+func TestBMPLib(t *testing.T) {
+	Convey("Testing `bmp lib` function", t, func() {
+		imgBytes, _ := os.ReadFile("rgb.bmp")
+		r := bytes.NewReader(imgBytes)
+
+		img, _ := bmp.Decode(r)
+		w := bytes.Buffer{}
+		_ = bmp.Encode(&w, img)
+
+		want := len(imgBytes)
+		got := len(w.Bytes())
+		So(got, ShouldResemble, want)
+	})
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module internshipApplicationTemplate
 
 go 1.17
 
-require github.com/smartystreets/goconvey v1.7.2
+require (
+	github.com/smartystreets/goconvey v1.7.2
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+)
 
 require (
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
