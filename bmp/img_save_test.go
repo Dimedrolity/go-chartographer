23a949177b2125295ca9765a478c178526853c9f
Dmitry Redkin <dima.redkin.1999@gmail.com>
2022-02-25 18:36:10 +0500

    cleanup

--- bmp/img_save_test.go
+++ bmp/img_save_test.go
@@ -1,7 +1,10 @@
 package bmp
 
 import (
+	"bytes"
 	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/image/bmp"
+	"os"
 	"testing"
 )
 
@@ -24,4 +27,22 @@ func TestAppendExt(t *testing.T) {
 	})
 }
 
-// интеграционный тест для проверка, что изображение корректно сохраняется на диск
+// TestBMPLib - интеграционный тест библиотеки "golang.org/x/image/bmp" для того, чтобы повысить доверие.
+// Библиотека должна кодировать/декодировать верно,
+// должны совпадать реальные с ожидаемыми: размер файла, размер изображений, пиксели - все байты.
+// TODO не использовать библиотеку bmp напрямую, только через публичное апи моего сервиса
+func TestBMPLib(t *testing.T) {
+	Convey("Testing `bmp lib` function", t, func() {
+		initialBytes, _ := os.ReadFile("rgb.bmp")
+		r := bytes.NewReader(initialBytes)
+
+		img, _ := bmp.Decode(r)
+		encodeBytes, _ := Encode(&img)
+
+		want := initialBytes
+		got := encodeBytes
+		So(got, ShouldResemble, want)
+	})
+}
+
+// нужна константная строка байтов, чтобы не зависеть от Ф.С.?
