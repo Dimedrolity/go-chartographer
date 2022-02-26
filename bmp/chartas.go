8ed48cabed6b1efabfb87f65bd094ed3e636b83b
Dmitry Redkin <dima.redkin.1999@gmail.com>
2022-02-26 20:48:57 +0500

    3 approaches, waiting for answer from author

--- bmp/chartas.go
+++ bmp/chartas.go
@@ -17,7 +17,7 @@ const (
 	maxHeight = 50_000
 )
 
-func NewImage(width int, height int) (image.Image, error) {
+func NewImage(width, height int) (image.Image, error) {
 	if width < minWidth {
 		return nil, errors.New(fmt.Sprintf("ошибка. Ширина изображения должна быть положительным числом.\n") +
 			fmt.Sprintf("Полученная ширина имеет значение %d", width))
@@ -38,4 +38,85 @@ func NewImage(width int, height int) (image.Image, error) {
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	return img, nil
+}
+
+// TODO w h д.б. целыми числами. В api делать преобразование в int. При ошибке возвращать ошибку, не вызывая SubImage
+
+const (
+	subImageMinWidth  = 1
+	subImageMinHeight = 1
+	subImageMaxWidth  = 5_000
+	subImageMaxHeight = 5_000
+)
+
+func SubImage(img image.Image, x, y, width, height int) (image.Image, error) {
+	if x < subImageMinWidth {
+		return nil, errors.New(fmt.Sprintf("ошибка. Ширина изображения должна быть положительным числом.\n") +
+			fmt.Sprintf("Полученная ширина имеет значение %d", width))
+	}
+	if y < subImageMinHeight {
+		return nil, errors.New(fmt.Sprintf("ошибка. Высота изображения должна быть положительным числом.\n") +
+			fmt.Sprintf("Полученная высота имеет значение %d", height))
+	}
+
+	if width < subImageMinWidth {
+		return nil, errors.New(fmt.Sprintf("ошибка. Ширина изображения должна быть положительным числом.\n") +
+			fmt.Sprintf("Полученная ширина имеет значение %d", width))
+	}
+	if height < subImageMinHeight {
+		return nil, errors.New(fmt.Sprintf("ошибка. Высота изображения должна быть положительным числом.\n") +
+			fmt.Sprintf("Полученная высота имеет значение %d", height))
+	}
+
+	if width > subImageMaxWidth {
+		return nil, errors.New(fmt.Sprintf("ошибка. Изображение превышает допустимый размер, максимально допустимая ширина = %d.\n", subImageMaxWidth) +
+			fmt.Sprintf("Полученная ширина имеет значение %d", width))
+	}
+	if height > subImageMaxHeight {
+		return nil, errors.New(fmt.Sprintf("ошибка. Изображение превышает допустимый размер, максимально допустимая высота = %d.\n", subImageMaxHeight) +
+			fmt.Sprintf("Полученная высота имеет значение %d", height))
+	}
+
+	// проверка что x, y содержится в img.Bounds (метод In)
+	//if !(Point{x, y}.In(p.Rect)) {
+	//	return color.RGBA64{}
+	//}
+
+	// проверка что w, h не превышают
+
+	// rgba.SubImage не подходит, так как работает через Intersect. По требованию нужен??
+	rgba, ok := img.(*image.RGBA)
+	if !ok {
+		return nil, errors.New("ошибка. Тип изображения не поддерживается")
+	}
+	sub := rgba.SubImage(image.Rect(x, y, x+width, y+height))
+
+	// будет ли черный по умолч?
+	// БУДЕТ ЛИ ВЕРНО Encode?
+
+	// Создание Rect с координат x y
+	//r := image.Rect(x, y, x+width, y+height)
+	//sub := image.NewRGBA(r)
+	//
+	//for h := 0; h < height; h++ {
+	//	for w := 0; w < width; w++ {
+	//		c := img.At(x+w, y+h)
+	//		sub.Set(x+w, y+h, c)
+	//	}
+	//}
+
+	// Создание Rect с координат 0 0
+	//sub := image.NewRGBA(image.Rect(0, 0, width, height))
+	//xSub := 0
+	//ySub := 0
+	//for yImg := 0; yImg < height; yImg++ {
+	//	for xImg := 0; xImg < width; xImg++ {
+	//		c := img.At(x+xImg, y+yImg)
+	//		sub.Set(xSub, ySub, c)
+	//		xSub++
+	//	}
+	//	ySub++
+	//}
+
+	return sub, nil
 }
--- bmp/chartas_test.go
+++ bmp/chartas_test.go
@@ -2,38 +2,40 @@ package bmp
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"image"
+	"image/color"
 	"testing"
 )
 
-func TestCreateImg_ZeroWidth(t *testing.T) {
+func TestNewImage_ZeroWidth(t *testing.T) {
 	Convey("ZeroWidth", t, func() {
 		_, err := NewImage(0, 1)
 		So(err, ShouldNotBeNil)
 	})
 }
 
-func TestCreateImg_ZeroHeight(t *testing.T) {
+func TestNewImage_ZeroHeight(t *testing.T) {
 	Convey("ZeroHeight", t, func() {
 		_, err := NewImage(1, 0)
 		So(err, ShouldNotBeNil)
 	})
 }
 
-func TestCreateImg_WidthExceeded(t *testing.T) {
+func TestNewImage_WidthExceeded(t *testing.T) {
 	Convey("WidthExceeded", t, func() {
 		_, err := NewImage(maxWidth+1, 1)
 		So(err, ShouldNotBeNil)
 	})
 }
 
-func TestCreateImg_HeightExceeded(t *testing.T) {
+func TestNewImage_HeightExceeded(t *testing.T) {
 	Convey("HeightExceeded", t, func() {
 		_, err := NewImage(1, maxHeight+1)
 		So(err, ShouldNotBeNil)
 	})
 }
 
-func TestCreateImg_MinMaxSize(t *testing.T) {
+func TestNewImage_MinMaxSize(t *testing.T) {
 	testSize := func(width, height int) {
 		img, err := NewImage(width, height)
 		So(err, ShouldBeNil)
@@ -49,3 +51,33 @@ func TestCreateImg_MinMaxSize(t *testing.T) {
 		testSize(maxWidth, maxHeight)
 	})
 }
+
+func TestSubImage(t *testing.T) {
+	Convey("SubImage", t, func() {
+		img := image.NewRGBA(image.Rect(0, 0, 2, 2))
+		red := color.RGBA{R: 255, G: 0, B: 0, A: 255}
+		const (
+			redX = 1
+			redY = 1
+		)
+		img.SetRGBA(redX, redY, red)
+
+		const (
+			subWidth  = 1
+			subHeight = 1
+		)
+		sub, err := SubImage(img, redX, redY, subWidth, subHeight)
+		So(err, ShouldBeNil)
+
+		bounds := sub.Bounds()
+		So(bounds.Dx(), ShouldEqual, subWidth)
+		So(bounds.Dy(), ShouldEqual, subHeight)
+
+		So(sub.At(redX, redY), ShouldResemble, red)
+	})
+}
+
+// TODO тест, когда SubImage выходит за рамки исходного
+// чёрным цветом закрашивается та часть фрагмента, которая оказывается вне границ изображения (см. пример ниже).
+// Получается нужно будет вернуть sub-image запрошенного размера, но часть будет черным цветом.
+// TODO уточнить у авторов.
