b8978d3e9a03c573c33c0a071801b954cc978665
Dmitry Redkin <dima.redkin.1999@gmail.com>
2022-03-13 20:26:49 +0500

    cleanup test

--- chart/img_save_test.go
+++ chart/img_save_test.go
@@ -14,25 +14,8 @@ import (
 // Выделить структуру ChartService, содержит путь к каталогу данных и все методы
 // Тогда нужно выделить интерфейс и методы сервиса? что даст?
 
-// TODO использовать public api
-//func TestGuid(t *testing.T) {
-//	Convey("guid", t, func() {
-//		id := guid()
-//		So(id, ShouldHaveLength, 36)
-//	})
-//}
-
-// TODO использовать public api
-//func TestAppendExt(t *testing.T) {
-//	Convey("appendExtension", t, func() {
-//		filename := appendExtension("qwerty")
-//		So(filename, ShouldEqual, "qwerty.bmp")
-//	})
-//}
-
 // TestDecodeEncode - интеграционный тест библиотеки "golang.org/x/image/bmp" для того, чтобы повысить доверие.
 // Библиотека должна кодировать/декодировать верно, должны совпадать все реальные байты с ожидаемыми.
-// TODO не использовать библиотеку bmp напрямую, только через публичное апи моего сервиса
 func TestDecodeEncode(t *testing.T) {
 	Convey("Байты изображения после Decode и Encode должны совпадать с исходными", t, func() {
 		const path = "testdata/rgb.bmp"
@@ -45,8 +28,10 @@ func TestDecodeEncode(t *testing.T) {
 		err = file.Close()
 		So(err, ShouldBeNil)
 
-		encodeBytes, err := store.Encode(img)
+		buffer := bytes.Buffer{}
+		err = bmp.Encode(&buffer, img)
 		So(err, ShouldBeNil)
+		encodeBytes := buffer.Bytes()
 
 		initialBytes, err := os.ReadFile(path)
 		So(err, ShouldBeNil)
