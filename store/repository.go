5adefd6dfc2e6c553414aa94cec81b11d8d68b05
Dmitry Redkin <dima.redkin.1999@gmail.com>
2022-03-19 15:56:51 +0500

    fix

--- store/repository.go
+++ store/repository.go
@@ -9,7 +9,7 @@ type TileRepository interface {
 	//по новой идее, x y не нужно передавать, будет вычисляться по imb.b.min
 	SaveTile(id string, x int, y int, img image.Image) error
 	// GetTile
-	// У возвращаемого image.Image Bounds().Min равен (0; 0).
+	// У возвращаемого image.Image Bounds().Min равен (x; y).
 	// TODO возвращать байты, Репо не должно кодировать и декодировать
 	GetTile(id string, x, y int) (image.Image, error)
 	DeleteImage(id string) error
